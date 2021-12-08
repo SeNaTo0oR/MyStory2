7b7bb5e44c0ef3b37e5fd899bda0e8e3ff4b9cbb
SeNaTo0oR <eng.fadi.daouk@gmail.com>
2021-12-08 20:22:27 +0300

    Showing email in Navigation Header

--- app/src/main/java/com/example/mystory/LoginActivity.kt
+++ app/src/main/java/com/example/mystory/LoginActivity.kt
@@ -3,10 +3,7 @@ package com.example.mystory
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.CheckBox
-import android.widget.EditText
-import android.widget.TextView
+import android.widget.*
 
 class LoginActivity : AppCompatActivity() {
     private var user:EditText? = null
@@ -20,7 +17,7 @@ class LoginActivity : AppCompatActivity() {
 
         connectViews()
         login()
-        forget()
+        //forget()
     }
 
     private fun connectViews() {
@@ -31,9 +28,37 @@ class LoginActivity : AppCompatActivity() {
         chAccept = findViewById(R.id.checkAccept)
     }
     private fun login(){
+        val arr:ArrayList<User> = ArrayList()
+        arr.add(User("test@test.com", "123"))
+        arr.add(User("t@t.com", "1234"))
+        arr.add(User("b@test.com", "12345"))
+
         btnLog?.setOnClickListener {
-            val i = Intent(this, LibraryActivity::class.java)
-            startActivity(i)
+            val username = user?.text.toString()
+            val password = pass?.text.toString()
+
+            val user = User(username,password)
+
+            for (userArray in arr){
+                if(userArray.email == user.email
+                    && userArray.password == user.password){
+                  //  Toast.makeText(this, "Welcome ${user.email}", Toast.LENGTH_SHORT).show()
+                      val i = Intent(this, MainActivity::class.java)
+                        i.putExtra("email", userArray.email)
+                        startActivity(i)
+                        break
+                }else{
+                    Toast.makeText(this, "check your data", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+//            if(user?.text?.isEmpty() == true
+//                || pass?.text?.isEmpty()== true
+//                ||chAccept?.isChecked != true){
+//                Toast.makeText(this, "Enter your data", Toast.LENGTH_SHORT).show()
+//            }
+//            val i = Intent(this, LibraryActivity::class.java)
+//            startActivity(i)
         }
     }
     private fun forget(){
--- app/src/main/java/com/example/mystory/MainActivity.kt
+++ app/src/main/java/com/example/mystory/MainActivity.kt
@@ -1,11 +1,79 @@
 package com.example.mystory
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
+    private var drawer: DrawerLayout? = null
+    private var toolbar: Toolbar?=null
+    private var navigation: NavigationView?=null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val email = intent.getStringExtra("email")
+
+        cntView()
+        setSupportActionBar(toolbar!!)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        setupDrawer()
+        navItemsClicks()
+        updateEmailInHeader(email!!)
+    }
+
+    private fun cntView() {
+        drawer = findViewById(R.id.drawerView)
+        toolbar = findViewById(R.id.toolBarView)
+        navigation = findViewById(R.id.navView)
+
+    }
+    private fun updateEmailInHeader(eamil:String){
+        val headerView = navigation?.getHeaderView(0)
+        val textHeaderView = headerView?.findViewById<TextView>(R.id.viewEmail)
+        textHeaderView?.text = eamil
+    }
+
+    private fun setupDrawer(){
+        val toggleDrawer: ActionBarDrawerToggle = ActionBarDrawerToggle(this, drawer!!,R.string.open,R.string.close)
+        drawer?.addDrawerListener(toggleDrawer)
+        toggleDrawer.syncState()
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            android.R.id.home -> {
+                drawer?.openDrawer(GravityCompat.START)
+                true
+            }else -> true
+        }
+    }
+    private fun navItemsClicks(){
+        navigation?.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.home -> {
+                    Toast.makeText(this,"Home Screen", Toast.LENGTH_SHORT).show()
+                    drawer?.closeDrawer(GravityCompat.START)
+                    true
+                }
+                R.id.logout -> {
+                    val i = Intent(this, LoginActivity::class.java)
+                    startActivity(i)
+//                    Toast.makeText(this,"Logged Out", Toast.LENGTH_SHORT).show()
+//                    drawer?.closeDrawer(GravityCompat.START)
+                    true
+                }else -> true
+            }
+        }
+
     }
 }
